70a3d9c3278298cefe0333c4d74d13321ca3dc92
mahmoud abdulhamid <mahmoudabdulhamid22@gmail.com>
2025-08-09 20:03:28 +0300

    Add runtime version file for Python 3.12

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,7 +1,4 @@
-import sys
-import os
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.vector_store import VectorStoreBuilder
 from src.recommender import AnimeRecommender
